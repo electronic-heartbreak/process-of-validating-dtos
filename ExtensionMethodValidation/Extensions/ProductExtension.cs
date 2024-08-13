c1a90bb528a2d29c4130dc45bd310d33656a0101
Bjorn <b.jaeken@student.avans.nl>
2024-08-13 15:46:01 +0200

    Improved validation logic.

--- ExtensionMethodValidation/Extensions/ProductExtension.cs
+++ ExtensionMethodValidation/Extensions/ProductExtension.cs
@@ -6,24 +6,20 @@ namespace ExtensionMethodValidation.Extensions
     {
         public static IEnumerable<string> Validate(this CreateProductDto productDto)
         {
-            var errors = new List<string>();
-
             if (string.IsNullOrWhiteSpace(productDto.Name))
             {
-                errors.Add("Name is required.");
+                yield return "Name is required";
             }
 
-            if (productDto.Name != null && productDto.Name.Length > 100)
+            if (productDto.Name.Length > 100)
             {
-                errors.Add("Name length can't be more than 100.");
+                yield return "Name length can't be more than 100";
             }
 
             if (productDto.Price < 0.01m || productDto.Price > 1000.00m)
             {
-                errors.Add("Price must be between 0.01 and 1000.00.");
+                yield return "Price must be between 0.01 and 1000.00";
             }
-
-            return errors;
         }
     }
 }
